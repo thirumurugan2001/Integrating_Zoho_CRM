03cc73cee35702148cb115a1093e0a7b8e3bbe69
VPearl-ai <riverpearlsolutions@gmail.com>
2025-10-24 13:05:19 +0530

    updated code fully

--- Integration.py
+++ Integration.py
@@ -3,43 +3,32 @@ from helper import excel_to_json,assign_sales_person_to_areas,separate_and_store
 
 def lead_import(file_path):
     try : 
-        file_path = separate_and_store_temp(file_path,True)
-        temp_path = assign_sales_person_to_areas(excel_file_path=file_path,area_column_name="Area Name", sales_person_column_name="Sales Person")
-        records = excel_to_json(temp_path) 
-        if records: 
-             return {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+        crm = ZohoCRMAutomatedAuth()    
+        if crm.test_api_connection():
+            crm.get_module_fields()
+            file_path = separate_and_store_temp(file_path,True)
+            temp_path = assign_sales_person_to_areas(excel_file_path=file_path,area_column_name="Area Name", sales_person_column_name="Sales Person")
+            records = excel_to_json(temp_path)   
+            if records:
+                success = crm.push_records_to_zoho(records)
+                if success:
+                    return {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
                         "message": "Records pushed successfully!",
                         "statusCode": 200,
                         "status": True,
                     }
-
-        # crm = ZohoCRMAutomatedAuth()    
-        # if crm.test_api_connection():
-        #     crm.get_module_fields()
-        #     file_path = separate_and_store_temp(file_path,True)
-        #     temp_path = assign_sales_person_to_areas(excel_file_path=file_path,area_column_name="Area Name", sales_person_column_name="Sales Person")
-        #     records = excel_to_json(temp_path)   
-        #     if records:
-        #         # success = crm.push_records_to_zoho(records)
-        #         success = True
-        #         if success:
-        #             return {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-        #                 "message": "Records pushed successfully!",
-        #                 "statusCode": 200,
-        #                 "status": True,
-        #             }
-        #         else:
-        #             return {
-        #                 "message": "Failed to push some or all records",
-        #                 "statusCode": 400,  
-        #                 "status": False,
-        #             }
-        #     else:
-        #         return {
-        #             "message": "No records found in Excel file",
-        #             "statusCode": 400,  
-        #             "status": False,
-        #         }
+                else:
+                    return {
+                        "message": "Failed to push some or all records",
+                        "statusCode": 400,  
+                        "status": False,
+                    }
+            else:
+                return {
+                    "message": "No records found in Excel file",
+                    "statusCode": 400,  
+                    "status": False,
+                }
         else:
             return {
                     "message": "API connection failed!",
